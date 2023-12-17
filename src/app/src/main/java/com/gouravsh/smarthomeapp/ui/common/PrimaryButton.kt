5aa9c7f7abf936dc6a1faf128546731bd66d0c3d
Shubham Rawat <shubhamrwt2001@gmail.com>
2023-12-17 22:40:20 +0530

    refactor: PrimaryButton.kt

--- src/app/src/main/java/com/gouravsh/smarthomeapp/ui/common/PrimaryButton.kt
+++ src/app/src/main/java/com/gouravsh/smarthomeapp/ui/common/PrimaryButton.kt
@@ -1,6 +1,5 @@
 package com.gouravsh.smarthomeapp.ui.common
 
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.ButtonDefaults
